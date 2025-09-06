df6387fb48bb21ed92c91868a1247396e596bb4c
lsqnick <37409246+lsqnick@users.noreply.github.com>
2025-09-06 16:17:54 +0900

    fix alias resolution

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ const moduleResolver = require('./babel-plugin-module-resolver');
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
-    [moduleResolver, { alias: { '@': './' } }]
+    [moduleResolver, { alias: { '@': '.' } }]
   ]
 };
--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.alias = {
-  '@': './',
+  '@': '.',
 };
 
 module.exports = config;
